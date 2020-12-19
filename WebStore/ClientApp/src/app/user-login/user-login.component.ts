538efb12a297b80adbb3b433ba36add4c440723a
Daniel Batista <danielfilipebatista@gmail.com>
2020-12-19 02:39:39 +0000

    it works

--- WebStore/ClientApp/src/app/user-login/user-login.component.ts
+++ WebStore/ClientApp/src/app/user-login/user-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-login',
+  selector: 'user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
